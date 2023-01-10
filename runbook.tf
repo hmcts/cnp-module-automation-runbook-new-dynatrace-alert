780c38f826f3d20d87252f7c77d84258458eae1a
Your Name <james.ferrari@version1.com>
2023-01-10 16:13:36 +0000

    amend input

--- runbook.tf
+++ runbook.tf
@@ -3,8 +3,8 @@ resource "azurerm_automation_runbook" "new_dynatrace_alert" {
   location                = var.location
   resource_group_name     = var.resource_group_name
   automation_account_name = var.use_existing_automation_account == true ? data.azurerm_automation_account.existing_automation_account[0].name : azurerm_automation_account.automation_account[0].name
-  log_verbose             = true
-  log_progress            = true
+  log_verbose             = var.log_verbose
+  log_progress            = var.log_progress
   description             = "This Runbook Creates a Dynatrace Alert using the Dynatrace REST API."
   runbook_type            = "PowerShell"
   content                 = file("${path.module}/New-DynatraceAlert.ps1")
