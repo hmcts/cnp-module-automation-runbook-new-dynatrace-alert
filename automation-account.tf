482ee2a398b0152d17196fc04d9b6f6c707b6e74
Your Name <james.ferrari@version1.com>
2023-03-14 14:33:36 +0000

    FIX: remove data source

--- automation-account.tf
+++
@@ -1,4 +0,0 @@
-data "azurerm_automation_account" "automation_account" {
-  name                = var.automation_account_name
-  resource_group_name = var.resource_group_name
-}
--- runbook.tf
+++ runbook.tf
@@ -2,7 +2,7 @@ resource "azurerm_automation_runbook" "new_dynatrace_alert" {
   name                    = "New-DynatraceAlert"
   location                = var.location
   resource_group_name     = var.resource_group_name
-  automation_account_name = data.azurerm_automation_account.automation_account.name
+  automation_account_name = var.automation_account_name
   log_verbose             = var.log_verbose
   log_progress            = var.log_progress
   description             = "This Runbook Creates a Dynatrace Alert using the Dynatrace API."
